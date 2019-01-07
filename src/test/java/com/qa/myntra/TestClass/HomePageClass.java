dad16e7cb3dd4c8ff3c0ae23b53957c81e2dba92
Dheerajps77 <39496037+Dheerajps77@users.noreply.github.com>
2019-01-07 12:50:58 +0530

    Adding Listener Class

--- src/test/java/com/qa/myntra/TestClass/HomePageClass.java
+++ src/test/java/com/qa/myntra/TestClass/HomePageClass.java
@@ -4,7 +4,9 @@ import java.io.IOException;
 import java.lang.reflect.Method;
 
 import org.openqa.selenium.support.PageFactory;
+import org.testng.Assert;
 import org.testng.ITestResult;
+import org.testng.SkipException;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -40,7 +42,7 @@ public class HomePageClass extends TestbaseClass{
 		{
 			e3.log(Status.FAIL, MarkupHelper.createLabel(result.getMethod().getMethodName(), ExtentColor.RED));
 			e3.log(Status.FAIL, result.getThrowable());
-			ab.ScreenshotCapture(driver, this.getClass().getSimpleName()+".png");
+			//ab.ScreenshotCapture(driver, this.getClass().getSimpleName()+".png");
 			e3.log(Status.FAIL, result.getMethod().getMethodName() + " has been failed");
 		}
 		
@@ -58,6 +60,57 @@ public class HomePageClass extends TestbaseClass{
 		e3.log(Status.INFO, "ClickOnGiftCard has been started");
 		ObjHomePage=PageFactory.initElements(driver, HomePage.class);
 		ObjHomePage.ClickOnGiftCard();
+	}
+	
+	@Test(priority=1)
+	public void ClickOnLoginButton()
+	{
+		e3.log(Status.INFO, "ClickOnLoginButton has been started");
+		Assert.assertEquals(true, false);
+	}
+	
+	@Test(priority=2)
+	public void ClickOnRegisterButton()
+	{
+		e3.log(Status.INFO, "ClickOnRegisterButton has been started");
+		Assert.assertEquals(true, true);
+	}
+	
+	@Test(priority=3)
+	public void ClickOnResetPasswordButton()
+	{
+		e3.log(Status.INFO, "ClickOnResetPasswordButton has been started");
+		Assert.assertEquals(true, false);
+	}
+	
+	@Test(priority=4)
+	public void ClickOnOrderNowButton()
+	{
+		e3.log(Status.INFO, "ClickOnOrderNowButton has been started");
+		Assert.assertEquals(true, true);
+		throw new SkipException("ClickOnOrderNowButton is not ready to test. Hence skipping this one.");
+	}
+	
+	@Test(priority=5)
+	public void ClickOnBuyNowButton()
+	{
+		e3.log(Status.INFO, "ClickOnBuyNowButton has been started");
+		Assert.assertEquals(true, true);
+	}
+	
+	@Test(priority=6)
+	public void ClickOnEMIButtonButton()
+	{
+		e3.log(Status.INFO, "ClickOnEMIButtonButton has been started");
+		Assert.assertEquals(true, true);
+		throw new SkipException("ClickOnEMIButtonButton is not ready to test. Hence skipping this one.");
+	}
+	
+	@Test(priority=7)
+	public void ClickOnSubmitButton()
+	{
+		e3.log(Status.INFO, "ClickOnSubmitButton has been started");
+		Assert.assertEquals(true, true);
 	}
 
 }
